16341947f02517919a0b7dd6787b8415119fd867
hugolopezjanquin <47836593+hugolopezjanquin@users.noreply.github.com>
2020-05-23 21:57:49 +0200

    Add files via upload

---
+++ PruebaPuertoSerie/arduinoPuerto.ino
@@ -0,0 +1,153 @@
+double Velocidad;
+
+int sensorDer;
+int sensorIzq;
+
+int ena=10;
+int in1=9;    /*der*/
+int in2=8;    /*der*/
+int in3=6;    /*izq*/
+int in4=7;    /*izq*/
+int enb=5;
+
+void avanzar();
+void pasilloDetectado();
+void girarDer();
+void girarIzq();
+void retroceder();
+
+int pasilloDeseado();
+void volverInicio(int);
+
+void setup() {
+  pinMode(ena, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  pinMode(enb, OUTPUT);
+  pinMode(11, INPUT);
+  pinMode(12, INPUT);
+  Velocidad = 80;
+  Serial.begin(9600);
+}
+
+void loop() {
+  int posicion;
+  posicion = pasilloDeseado();
+  volverInicio(posicion);
+}
+
+void avanzar(){
+    digitalWrite(in3,LOW);
+    digitalWrite(in4,HIGH);
+    analogWrite(enb,(Velocidad));
+    digitalWrite(in1,HIGH);
+    digitalWrite(in2,LOW);
+    analogWrite(ena,(Velocidad));
+}
+
+void girarDer(){
+    digitalWrite(in3,LOW);
+    digitalWrite(in4,HIGH);
+    analogWrite(enb,(Velocidad));
+    digitalWrite(in1,HIGH);
+    digitalWrite(in2,LOW);
+    analogWrite(ena,(0));
+}
+
+void girarIzq(){
+  digitalWrite(in1,HIGH);
+    digitalWrite(in2,LOW);
+    analogWrite(ena,(Velocidad));
+    digitalWrite(in3,LOW);
+    digitalWrite(in4,HIGH);
+    analogWrite(enb,(0));
+}
+
+void pasilloDetectado(){
+    digitalWrite(in3,LOW);
+    digitalWrite(in4,HIGH);
+    analogWrite(enb,(0));
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,HIGH);
+    analogWrite(ena,(0));
+}
+
+void retroceder(){
+    digitalWrite(in4,LOW);
+    digitalWrite(in3,HIGH);
+    analogWrite(enb,(Velocidad));
+    digitalWrite(in2,HIGH);
+    digitalWrite(in1,LOW);
+    analogWrite(ena,(Velocidad));
+}
+
+int pasilloDeseado(){
+   int pasillo=0, contador;
+   contador=0;
+  while(!Serial.available());
+    contador--;
+    pasillo = Serial.read();
+   
+    while(contador != pasillo){
+      sensorDer = digitalRead(11);                   /*cable blanco*/
+      sensorIzq = digitalRead(12);                   /*cable verde*/
+      if(sensorDer == 0 && sensorIzq == 0){          //no detectan nada empieza a moverse
+          avanzar();
+      }else if(sensorDer == 1 && sensorIzq == 0 && contador != pasillo){        //detecta derecha gira izquierda
+          delay(400);        //delay para evitar fallos de lectura y que el robot no gire demasiado
+          if(sensorDer == 1 && sensorIzq == 0){
+            girarDer();
+          }
+      }else if(sensorDer == 0 && sensorIzq == 1 && contador != pasillo){        //detecta izq gira der
+          delay(400);        //delay para evitar fallos de lectura y que el robot no gire demasiado
+          if(sensorDer == 0 && sensorIzq == 1){
+            girarIzq();
+          }
+      }else if(sensorDer == 1 && sensorIzq == 1 && contador != pasillo){        //detecta que ha llegado pasillo, como no es el pasillo que queremos continua
+          contador++;
+          delay(550);       //delay para evitar que el contador aumente de forma excesiva
+          avanzar();
+          if(contador == pasillo){          //si llega al pasillo deseado se para
+          pasilloDetectado();
+          //Serial.write(contador);   //manda el contador a C para decir que ha acabado
+  }
+      }
+  }
+  return contador;
+}
+
+
+void volverInicio(int pasillo){
+    int contador, inicio = -1;
+    contador = pasillo;
+  while(!Serial.available());
+    pasillo = Serial.read();
+    
+    while(contador != inicio){
+      sensorDer = digitalRead(11);                   /*cable blanco*/
+      sensorIzq = digitalRead(12);                   /*cable verde*/
+      if(sensorDer == 0 && sensorIzq == 0){          //no detectan nada empieza a moverse
+          retroceder();
+      }else if(sensorDer == 0 && sensorIzq == 1 && contador != inicio){        //detecta derecha gira izquierda
+          delay(400);        //delay para evitar fallos de lectura y que el robot no gire demasiado
+          if(sensorDer == 0 && sensorIzq == 1){
+            girarDer();
+          }
+      }else if(sensorDer == 1 && sensorIzq == 0 && contador != inicio){        //detecta izq gira der
+          delay(400);        //delay para evitar fallos de lectura y que el robot no gire demasiado
+          if(sensorDer == 1 && sensorIzq == 0){
+            girarIzq();
+          }
+      }else if(sensorDer == 1 && sensorIzq == 1 && contador != inicio){        //detecta que ha llegado pasillo, como no es el pasillo que queremos continua
+          contador--;
+          delay(550);       //delay para evitar que el contador aumente de forma excesiva
+          avanzar();
+          if(contador == inicio){          //si llega al pasillo deseado se para
+          pasilloDetectado();
+          //Serial.write(contador);   //manda el contador a C para decir que ha acabado
+  }
+      }
+  }
+}
